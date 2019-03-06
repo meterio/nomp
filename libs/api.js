b2fb14cca12a94c1b88852f9bac15efca47b5b73
simonzg <zhanghan.simon@gmail.com>
2019-03-06 12:26:21 -0800

    added display for height/miner/timestamp

--- libs/api.js
+++ libs/api.js
@@ -1,56 +1,57 @@
-var redis = require('redis');
-var async = require('async');
+var redis = require("redis");
+var async = require("async");
 
-var stats = require('./stats.js');
+var stats = require("./stats.js");
 
-module.exports = function(logger, portalConfig, poolConfigs){
+module.exports = function(logger, portalConfig, poolConfigs) {
+  var _this = this;
 
+  var portalStats = (this.stats = new stats(logger, portalConfig, poolConfigs));
 
-    var _this = this;
+  this.liveStatConnections = {};
 
-    var portalStats = this.stats = new stats(logger, portalConfig, poolConfigs);
+  this.handleApiRequest = async function(req, res, next) {
+    switch (req.params.method) {
+      case "stats":
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(portalStats.statsString);
+        return;
+      case "pool_stats":
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(portalStats.statPoolHistory));
+        return;
+      case "live_stats":
+        res.writeHead(200, {
+          "Content-Type": "text/event-stream",
+          "Cache-Control": "no-cache",
+          Connection: "keep-alive"
+        });
+        res.write("\n");
+        var uid = Math.random().toString();
+        _this.liveStatConnections[uid] = res;
+        req.on("close", function() {
+          delete _this.liveStatConnections[uid];
+        });
 
-    this.liveStatConnections = {};
+        return;
+      case "blocks":
+        var data = await portalStats.getBlockHistory();
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(data));
+        return;
+      default:
+        next();
+    }
+  };
 
-    this.handleApiRequest = function(req, res, next){
-        switch(req.params.method){
-            case 'stats':
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(portalStats.statsString);
-                return;
-            case 'pool_stats':
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(portalStats.statPoolHistory));
-                return;
-            case 'live_stats':
-                res.writeHead(200, {
-                    'Content-Type': 'text/event-stream',
-                    'Cache-Control': 'no-cache',
-                    'Connection': 'keep-alive'
-                });
-                res.write('\n');
-                var uid = Math.random().toString();
-                _this.liveStatConnections[uid] = res;
-                req.on("close", function() {
-                    delete _this.liveStatConnections[uid];
-                });
-
-                return;
-            default:
-                next();
-        }
-    };
-
-
-    this.handleAdminApiRequest = function(req, res, next){
-        switch(req.params.method){
-            case 'pools': {
-                res.end(JSON.stringify({result: poolConfigs}));
-                return;
-            }
-            default:
-                next();
-        }
-    };
-
+  this.handleAdminApiRequest = function(req, res, next) {
+    switch (req.params.method) {
+      case "pools": {
+        res.end(JSON.stringify({ result: poolConfigs }));
+        return;
+      }
+      default:
+        next();
+    }
+  };
 };
--- website/static/stats.js
+++ website/static/stats.js
@@ -9,194 +9,270 @@ var poolBlockChart;
 var statData;
 var poolKeys;
 
-function buildChartData(){
+function buildChartData() {
+  var pools = {};
 
-    var pools = {};
-
-    poolKeys = [];
-    for (var i = 0; i < statData.length; i++){
-        for (var pool in statData[i].pools){
-            if (poolKeys.indexOf(pool) === -1)
-                poolKeys.push(pool);
-        }
+  poolKeys = [];
+  for (var i = 0; i < statData.length; i++) {
+    for (var pool in statData[i].pools) {
+      if (poolKeys.indexOf(pool) === -1) poolKeys.push(pool);
     }
+  }
 
+  for (var i = 0; i < statData.length; i++) {
+    var time = statData[i].time * 1000;
 
-    for (var i = 0; i < statData.length; i++){
-
-        var time = statData[i].time * 1000;
-
-        for (var f = 0; f < poolKeys.length; f++){
-            var pName = poolKeys[f];
-            var a = pools[pName] = (pools[pName] || {
-                hashrate: [],
-                workers: [],
-                blocks: []
-            });
-            if (pName in statData[i].pools){
-                a.hashrate.push([time, statData[i].pools[pName].hashrate]);
-                a.workers.push([time, statData[i].pools[pName].workerCount]);
-                a.blocks.push([time, statData[i].pools[pName].blocks.pending])
-            }
-            else{
-                a.hashrate.push([time, 0]);
-                a.workers.push([time, 0]);
-                a.blocks.push([time, 0])
-            }
-
-        }
-
+    for (var f = 0; f < poolKeys.length; f++) {
+      var pName = poolKeys[f];
+      var a = (pools[pName] = pools[pName] || {
+        hashrate: [],
+        workers: [],
+        blocks: []
+      });
+      if (pName in statData[i].pools) {
+        a.hashrate.push([time, statData[i].pools[pName].hashrate]);
+        a.workers.push([time, statData[i].pools[pName].workerCount]);
+        a.blocks.push([time, statData[i].pools[pName].blocks.pending]);
+      } else {
+        a.hashrate.push([time, 0]);
+        a.workers.push([time, 0]);
+        a.blocks.push([time, 0]);
+      }
     }
+  }
 
-    poolWorkerData = [];
-    poolHashrateData = [];
-    poolBlockData = [];
+  poolWorkerData = [];
+  poolHashrateData = [];
+  poolBlockData = [];
 
-    for (var pool in pools){
-        poolWorkerData.push({
-            key: pool,
-            values: pools[pool].workers
-        });
-        poolHashrateData.push({
-            key: pool,
-            values: pools[pool].hashrate
-        });
-        poolBlockData.push({
-            key: pool,
-            values: pools[pool].blocks
-        })
-    }
+  for (var pool in pools) {
+    poolWorkerData.push({
+      key: pool,
+      values: pools[pool].workers
+    });
+    poolHashrateData.push({
+      key: pool,
+      values: pools[pool].hashrate
+    });
+    poolBlockData.push({
+      key: pool,
+      values: pools[pool].blocks
+    });
+  }
 }
 
-function getReadableHashRateString(hashrate){
-    var i = -1;
-    var byteUnits = [ ' KH', ' MH', ' GH', ' TH', ' PH' ];
-    do {
-        hashrate = hashrate / 1024;
-        i++;
-    } while (hashrate > 1024);
-    return Math.round(hashrate) + byteUnits[i];
+function getReadableHashRateString(hashrate) {
+  var i = -1;
+  var byteUnits = [" KH", " MH", " GH", " TH", " PH"];
+  do {
+    hashrate = hashrate / 1024;
+    i++;
+  } while (hashrate > 1024);
+  return Math.round(hashrate) + byteUnits[i];
 }
 
-function timeOfDayFormat(timestamp){
-    var dStr = d3.time.format('%I:%M %p')(new Date(timestamp));
-    if (dStr.indexOf('0') === 0) dStr = dStr.slice(1);
-    return dStr;
+function timeOfDayFormat(timestamp) {
+  var dStr = d3.time.format("%I:%M %p")(new Date(timestamp));
+  if (dStr.indexOf("0") === 0) dStr = dStr.slice(1);
+  return dStr;
 }
 
-function displayCharts(){
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != "undefined" ? args[number] : match;
+    });
+  };
+}
+function pad(n, l) {
+  return String("00" + n).slice(-1 * l);
+}
+function pad_space(n, l) {
+  return String("    " + n).slice(-1 * l);
+}
 
-    nv.addGraph(function() {
-        poolWorkerChart = nv.models.stackedAreaChart()
-            .margin({left: 40, right: 40})
-            .x(function(d){ return d[0] })
-            .y(function(d){ return d[1] })
-            .useInteractiveGuideline(true)
-            .clipEdge(true);
+function formatDate(date) {
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
 
-        poolWorkerChart.xAxis.tickFormat(timeOfDayFormat);
+  return String.format(
+    "{0}-{1}-{2} {3}:{4}:{5}",
+    pad(monthIndex + 1, 2),
+    pad(day, 2),
+    pad(year, 4),
+    pad(hour, 2),
+    pad(min, 2),
+    pad(sec, 2)
+  );
+}
 
-        poolWorkerChart.yAxis.tickFormat(d3.format('d'));
+function displayBlocks(blocks) {
+  var elems = blocks.map(function(b) {
+    var date = new Date(0);
+    date.setUTCMilliseconds(b.time);
+    var line = String.format(
+      "<p class='line'><span class='unit'><b>height</b>: {0}</span> <span class='unit'><b>miner</b>: {1}</span> at <span class='unit'>{2}</span></p>",
+      pad_space(b.height, 5),
+      b.addr,
+      formatDate(date)
+    );
+    return $(line);
+  });
+  console.log(elems);
+  root = $("<div/>");
+  for (var i in elems) {
+    root.append(elems[i]);
+  }
+  console.log("ROOT");
+  console.log(root);
+  $("#blocks").html("");
+  $("#blocks").append(root);
+}
 
-        d3.select('#poolWorkers').datum(poolWorkerData).call(poolWorkerChart);
+function displayCharts() {
+  nv.addGraph(function() {
+    poolWorkerChart = nv.models
+      .stackedAreaChart()
+      .margin({ left: 40, right: 40 })
+      .x(function(d) {
+        return d[0];
+      })
+      .y(function(d) {
+        return d[1];
+      })
+      .useInteractiveGuideline(true)
+      .clipEdge(true);
 
-        return poolWorkerChart;
+    poolWorkerChart.xAxis.tickFormat(timeOfDayFormat);
+
+    poolWorkerChart.yAxis.tickFormat(d3.format("d"));
+
+    d3.select("#poolWorkers")
+      .datum(poolWorkerData)
+      .call(poolWorkerChart);
+
+    return poolWorkerChart;
+  });
+
+  nv.addGraph(function() {
+    poolHashrateChart = nv.models
+      .lineChart()
+      .margin({ left: 60, right: 40 })
+      .x(function(d) {
+        return d[0];
+      })
+      .y(function(d) {
+        return d[1];
+      })
+      .useInteractiveGuideline(true);
+
+    poolHashrateChart.xAxis.tickFormat(timeOfDayFormat);
+
+    poolHashrateChart.yAxis.tickFormat(function(d) {
+      return getReadableHashRateString(d);
     });
 
+    d3.select("#poolHashrate")
+      .datum(poolHashrateData)
+      .call(poolHashrateChart);
 
-    nv.addGraph(function() {
-        poolHashrateChart = nv.models.lineChart()
-            .margin({left: 60, right: 40})
-            .x(function(d){ return d[0] })
-            .y(function(d){ return d[1] })
-            .useInteractiveGuideline(true);
+    return poolHashrateChart;
+  });
 
-        poolHashrateChart.xAxis.tickFormat(timeOfDayFormat);
+  nv.addGraph(function() {
+    poolBlockChart = nv.models
+      .multiBarChart()
+      .x(function(d) {
+        return d[0];
+      })
+      .y(function(d) {
+        return d[1];
+      });
 
-        poolHashrateChart.yAxis.tickFormat(function(d){
-            return getReadableHashRateString(d);
-        });
+    poolBlockChart.xAxis.tickFormat(timeOfDayFormat);
 
-        d3.select('#poolHashrate').datum(poolHashrateData).call(poolHashrateChart);
+    poolBlockChart.yAxis.tickFormat(d3.format("d"));
 
-        return poolHashrateChart;
-    });
+    d3.select("#poolBlocks")
+      .datum(poolBlockData)
+      .call(poolBlockChart);
 
-
-    nv.addGraph(function() {
-        poolBlockChart = nv.models.multiBarChart()
-            .x(function(d){ return d[0] })
-            .y(function(d){ return d[1] });
-
-        poolBlockChart.xAxis.tickFormat(timeOfDayFormat);
-
-        poolBlockChart.yAxis.tickFormat(d3.format('d'));
-
-        d3.select('#poolBlocks').datum(poolBlockData).call(poolBlockChart);
-
-        return poolBlockChart;
-    });
+    return poolBlockChart;
+  });
 }
 
-function TriggerChartUpdates(){
-    poolWorkerChart.update();
-    poolHashrateChart.update();
-    poolBlockChart.update();
+function TriggerChartUpdates() {
+  poolWorkerChart.update();
+  poolHashrateChart.update();
+  poolBlockChart.update();
 }
 
 nv.utils.windowResize(TriggerChartUpdates);
 
-$.getJSON('/api/pool_stats', function(data){
-    statData = data;
-    buildChartData();
-    displayCharts();
+$.getJSON("/api/pool_stats", function(data) {
+  statData = data;
+  buildChartData();
+  displayCharts();
 });
 
-statsSource.addEventListener('message', function(e){
-    var stats = JSON.parse(e.data);
-    statData.push(stats);
+$.getJSON("/api/blocks", function(data) {
+  displayBlocks(data);
+});
 
+statsSource.addEventListener("message", function(e) {
+  var stats = JSON.parse(e.data);
+  statData.push(stats);
 
-    var newPoolAdded = (function(){
-        for (var p in stats.pools){
-            if (poolKeys.indexOf(p) === -1)
-                return true;
-        }
-        return false;
-    })();
-
-    if (newPoolAdded || Object.keys(stats.pools).length > poolKeys.length){
-        buildChartData();
-        displayCharts();
+  var newPoolAdded = (function() {
+    for (var p in stats.pools) {
+      if (poolKeys.indexOf(p) === -1) return true;
     }
-    else {
-        var time = stats.time * 1000;
-        for (var f = 0; f < poolKeys.length; f++) {
-            var pool =  poolKeys[f];
-            for (var i = 0; i < poolWorkerData.length; i++) {
-                if (poolWorkerData[i].key === pool) {
-                    poolWorkerData[i].values.shift();
-                    poolWorkerData[i].values.push([time, pool in stats.pools ? stats.pools[pool].workerCount : 0]);
-                    break;
-                }
-            }
-            for (var i = 0; i < poolHashrateData.length; i++) {
-                if (poolHashrateData[i].key === pool) {
-                    poolHashrateData[i].values.shift();
-                    poolHashrateData[i].values.push([time, pool in stats.pools ? stats.pools[pool].hashrate : 0]);
-                    break;
-                }
-            }
-            for (var i = 0; i < poolBlockData.length; i++) {
-                if (poolBlockData[i].key === pool) {
-                    poolBlockData[i].values.shift();
-                    poolBlockData[i].values.push([time, pool in stats.pools ? stats.pools[pool].blocks.pending : 0]);
-                    break;
-                }
-            }
+    return false;
+  })();
+
+  if (newPoolAdded || Object.keys(stats.pools).length > poolKeys.length) {
+    buildChartData();
+    displayCharts();
+  } else {
+    var time = stats.time * 1000;
+    for (var f = 0; f < poolKeys.length; f++) {
+      var pool = poolKeys[f];
+      for (var i = 0; i < poolWorkerData.length; i++) {
+        if (poolWorkerData[i].key === pool) {
+          poolWorkerData[i].values.shift();
+          poolWorkerData[i].values.push([
+            time,
+            pool in stats.pools ? stats.pools[pool].workerCount : 0
+          ]);
+          break;
         }
-        TriggerChartUpdates();
+      }
+      for (var i = 0; i < poolHashrateData.length; i++) {
+        if (poolHashrateData[i].key === pool) {
+          poolHashrateData[i].values.shift();
+          poolHashrateData[i].values.push([
+            time,
+            pool in stats.pools ? stats.pools[pool].hashrate : 0
+          ]);
+          break;
+        }
+      }
+      for (var i = 0; i < poolBlockData.length; i++) {
+        if (poolBlockData[i].key === pool) {
+          poolBlockData[i].values.shift();
+          poolBlockData[i].values.push([
+            time,
+            pool in stats.pools ? stats.pools[pool].blocks.pending : 0
+          ]);
+          break;
+        }
+      }
     }
-
-
+    TriggerChartUpdates();
+  }
 });
